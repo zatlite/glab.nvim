6fee80664c1494324a9d8a22537fe752e456e7f8
Aung T. T. Kyaw <zatlite@gmail.com>
2024-02-28 22:06:34 +1100

    💥 refactored browse method to accept options and handle line selection

--- lua/glab/init.lua
+++ lua/glab/init.lua
@@ -25,22 +25,36 @@ local get_project_path = function()
 	return string.match(url, "[a-z]/(.-).git$")
 end
 
-M.browse = function()
-	local currentBranch, _ = utils.get_os_command_output({ "git", "branch", "--show-current" }, ".")
+M.browse = function(option, get_selection)
+	get_selection = get_selection or false
+	local path = option
+	if option == "current_branch" then
+		local result, _ = utils.get_os_command_output({ "git", "branch", "--show-current" }, ".")
+		path = result[1]
+	elseif option == "current_commit" then
+		local result, _ = utils.get_os_command_output({ "git", "rev-parse", "--short", "HEAD" }, ".")
+		path = result[1]
+	end
+	local line_nums = ""
+	if get_selection then
+		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", false, true, true), "nx", false) -- Need to escape visual mode for marks to refresh
+		local from = vim.api.nvim_buf_get_mark(0, "<")
+		local to = vim.api.nvim_buf_get_mark(0, ">")
+		line_nums = "\\#L" .. from[1] .. "-" .. to[1]
+	end
+
 	vim.cmd(
-		"!open "
+		"!open '"
 			.. state.GITLAB_URL
 			.. "/"
 			.. get_project_path()
 			.. "/-/blob/"
-			.. currentBranch[1]
+			.. path
 			.. "/"
 			.. vim.fn.expand("%:.")
+			.. line_nums
+			.. "'"
 	)
-end
-
-M.browseMaster = function()
-	vim.cmd("!open " .. state.GITLAB_URL .. "/" .. get_project_path() .. "/-/blob/master/" .. vim.fn.expand("%:."))
 end
 
 M.checkout = function()
