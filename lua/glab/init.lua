d9a9af9d6bf7953f98a589fd489732cf6545ac57
Aung T. T. Kyaw <zatlite@gmail.com>
2024-04-15 13:11:07 +1000

    ✨ method to open MR in browser

--- lua/glab/init.lua
+++ lua/glab/init.lua
@@ -112,6 +112,64 @@ M.checkout = function()
 	branch_picker(require("telescope.themes").get_dropdown({}))
 end
 
+M.browse_mr = function()
+	local json = {
+		query = 'query { project(fullPath: "'
+			.. get_project_path()
+			.. '") { mergeRequests(state: opened) { nodes { reference title sourceBranch webUrl }}}}',
+	}
+	local post_res = curl.post(state.GITLAB_URL .. "/api/graphql", {
+		accept = "application/json",
+		body = vim.fn.json_encode(json),
+		headers = {
+			content_type = "application/json",
+			authorization = "Bearer " .. state.GITLAB_TOKEN,
+		},
+	})
+
+	local nodes = vim.fn.json_decode(post_res.body).data.project.mergeRequests.nodes
+	local list = {}
+	for i, node in ipairs(nodes) do
+		table.insert(
+			list,
+			i,
+			{ string.format("%s: %s", string.gsub(node.reference, "!", ""), node.title), node.webUrl }
+		)
+	end
+
+	local branch_picker = function(opts)
+		opts = opts or {}
+		pickers
+			.new(opts, {
+				prompt_title = "Browse merge request",
+
+				finder = finders.new_table({
+					results = list,
+					entry_maker = function(entry)
+						return {
+							value = entry[2],
+							display = entry[1],
+							ordinal = entry[1],
+						}
+					end,
+				}),
+				sorter = conf.generic_sorter(opts),
+				attach_mappings = function(prompt_bufnr, map)
+					actions.select_default:replace(function()
+						actions.close(prompt_bufnr)
+						local selection = action_state.get_selected_entry()
+						vim.cmd("!open " .. selection.value)
+					end)
+					return true
+				end,
+			})
+			:find()
+	end
+
+	-- to execute the function
+	branch_picker(require("telescope.themes").get_dropdown({}))
+end
+
 M.yank_merge_request = function()
 	local json = {
 		query = 'query { project(fullPath: "'
